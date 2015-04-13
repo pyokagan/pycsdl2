f377ec5d4a18404aacd776bd07f48c10eb9e6cd4
Paul Tan <pyokagan@pyokagan.name>
2015-04-13 13:28:00 +0800

    csdl2.c: introduce bare extension source file
    csdl2.c includes the boilerplate to implement a bare CPython extension
    with no methods. The extension can thus be compiled on Linux with:
    
        c89 -fPIC -shared `pkg-config --cflags --libs python-3.4` \
        -o csdl2.so src/csdl2.c

---
+++ src/csdl2.c
@@ -0,0 +1,56 @@
+/*
+ * pycsdl2
+ * Copyright (c) 2015 Paul Tan <pyokagan@pyokagan.name>
+ *
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from
+ * the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *     1. The origin of this software must not be misrepresented; you must
+ *        not claim that you wrote the original software. If you use this
+ *        software in a product, an acknowledgment in the product
+ *        documentation would be appreciated but is not required.
+ *     2. Altered source versions must be plainly marked as such, and must
+ *        not be misrepresented as being the original software.
+ *     3. This notice may not be removed or altered from any source
+ *        distribution.
+ */
+/**
+ * \file csdl2.c
+ * \brief csdl2 extension source code.
+ */
+#include <Python.h>
+#include "../include/pycsdl2.h"
+
+/**
+ * \brief csdl2 module definition.
+ *
+ * \sa PyInit_csdl2()
+ */
+static PyModuleDef PyCSDL2_Module = {
+    PyModuleDef_HEAD_INIT,
+    /* m_name */ "csdl2",
+    /* m_doc */ "Simple DirectMedia Layer",
+    /* m_size */ -1,
+    /* m_methods */ NULL,
+    /* m_reload */ NULL,
+    /* m_traverse */ NULL,
+    /* m_clear */ NULL,
+    /* m_free */ NULL
+};
+
+/**
+ * \brief csdl2 module init entry point.
+ */
+PyMODINIT_FUNC
+PyInit_csdl2(void)
+{
+    PyObject *m = PyModule_Create(&PyCSDL2_Module);
+
+    if (!m) { return NULL; }
+    return m;
+}
