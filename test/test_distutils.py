ac005b6d90026fc79e64dc4f8dca71ac45c12336
Paul Tan <pyokagan@pyokagan.name>
2015-04-14 21:23:13 +0800

    test_distutils: test interface of PyCSDL2_GetSystemSDL()
    PyCSDL2_GetSystemSDL() must return either a dict or None.
    
    If a dict is returned,
    
    * It must only have the keys 'include_dirs', 'define_macros',
      'undef_macros', 'extra_compile_args', 'library_dirs', 'libraries',
      'runtime_library_dirs' and 'extra_link_args'.
    
    * Values of 'include_dirs', 'define_macros', 'undef_macros',
      'extra_compile_args', 'library_dirs', 'libraries',
      'runtime_library_dirs' and 'extra_link_args' must be a list of
      strings.
    
    * Value of 'define_macros' must be a list of 2-tuples of (str, str or
      None).

--- test/test_distutils.py
+++ test/test_distutils.py
@@ -15,5 +15,69 @@ if __name__ == '__main__':
 from csdl2 import *
 
 
+class TestGetSystemSDL(unittest.TestCase):
+    """Test the interface of PyCSDL2_GetSystemSDL()"""
+
+    def test_return_value(self):
+        "Must return either a dict or None"
+        ret = PyCSDL2_GetSystemSDL()
+        if ret is not None:
+            self.assertIs(type(ret), dict)
+
+    def test_return_value_system(self):
+        """When a dict is returned, it must have the expected keys and values.
+
+        It must only have the keys 'include_dirs', 'define_macros',
+        'undef_macros', 'extra_compile_args', 'library_dirs', 'libraries',
+        'runtime_library_dirs' and 'extra_link_args'.
+
+        Values of 'include_dirs', 'define_macros', 'undef_macros',
+        'extra_compile_args', 'library_dirs', 'libraries',
+        'runtime_library_dirs' and 'extra_link_args' must be a list of strings.
+
+        Value of 'define_macros' must be a list of 2-tuples of (str, str or
+        None).
+        """
+        ret = PyCSDL2_GetSystemSDL()
+        if ret is None:
+            raise unittest.SkipTest('csdl2 not dynamically linked')
+        self.assertEqual(len(ret), 8)
+
+        self.assertIs(type(ret['include_dirs']), list)
+        for x in ret['include_dirs']:
+            self.assertIs(type(x), str)
+
+        self.assertIs(type(ret['define_macros']), list)
+        for x in ret['define_macros']:
+            self.assertIs(type(x), tuple)
+            self.assertEqual(len(x), 2)
+            self.assertIs(type(x[0]), str)
+            self.assertTrue(type(x[1]) is str or x[1] is None)
+
+        self.assertIs(type(ret['undef_macros']), list)
+        for x in ret['undef_macros']:
+            self.assertIs(type(x), str)
+
+        self.assertIs(type(ret['extra_compile_args']), list)
+        for x in ret['extra_compile_args']:
+            self.assertIs(type(x), str)
+
+        self.assertIs(type(ret['library_dirs']), list)
+        for x in ret['library_dirs']:
+            self.assertIs(type(x), str)
+
+        self.assertIs(type(ret['libraries']), list)
+        for x in ret['libraries']:
+            self.assertIs(type(x), str)
+
+        self.assertIs(type(ret['runtime_library_dirs']), list)
+        for x in ret['runtime_library_dirs']:
+            self.assertIs(type(x), str)
+
+        self.assertIs(type(ret['extra_link_args']), list)
+        for x in ret['extra_link_args']:
+            self.assertIs(type(x), str)
+
+
 if __name__ == '__main__':
     unittest.main()
