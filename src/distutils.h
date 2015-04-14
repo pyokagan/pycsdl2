3de7d4a1edbf298a86c06833431cbbad7169bea3
Paul Tan <pyokagan@pyokagan.name>
2015-04-14 18:34:05 +0800

    distutils.h: introduce PyCSDL2_GetSystemSDLIncludeDirs()
    To save the list of distutils.extension.Extension.include_dirs that is
    required to compile the csdl2 extension against the system SDL2 library,
    we introduce:
    
    * the PYCSDL2_INCLUDE_DIRS macro, which should be defined in
      a compile flag with the value of include_dirs
    
    * PyCSDL2_GetSystemSDLIncludeDirs() function to retrieve the saved value
      of PYCSDL2_INCLUDE_DIRS as a PyListObject.

--- src/distutils.h
+++ src/distutils.h
@@ -31,4 +31,48 @@
 #include <Python.h>
 #include "../include/pycsdl2.h"
 
+#ifndef PYCSDL2_INCLUDE_DIRS
+/**
+ * \brief \c include_dirs used to find the system SDL2 header files when
+ *        compiling pycsdl2.
+ *
+ * A comma-separated list of C string literals that must end with a
+ * comma. Each C string literal is a include directory that can be used to find
+ * the system SDL2 header files and their dependencies. This corresponds to the
+ * \c distutils.extension.Extension.include_dirs attribute. If the list has no
+ * elements, either leave the macro empty or do not define this macro.
+ */
+#define PYCSDL2_INCLUDE_DIRS
+#endif /* PYCSDL2_INCLUDE_DIRS */
+
+/**
+ * \brief Return PyListObject of PYCSDL2_INCLUDE_DIRS.
+ *
+ * \return PyListObject of PYCSDL2_INCLUDE_DIRS if defined, else an empty
+ *         PyListObject. Returns NULL if an exception occurred.
+ */
+static PyObject *
+PyCSDL2_GetSystemSDLIncludeDirs(void)
+{
+    static const char *include_dirs[] = {PYCSDL2_INCLUDE_DIRS NULL};
+    PyObject *list;
+    Py_ssize_t i, len;
+
+    /* Calculate len of include_dirs */
+    for (len = 0; include_dirs[len] != NULL; ++len) {}
+    /* Create output list */
+    if (!(list = PyList_New(len)))
+        return NULL;
+    /* Set items on list */
+    for (i = 0; include_dirs[i] != NULL; ++i) {
+        PyObject *str = PyUnicode_FromString(include_dirs[i]);
+        if (str == NULL) {
+            Py_DECREF(list);
+            return NULL;
+        }
+        PyList_SET_ITEM(list, i, str);
+    }
+    return list;
+}
+
 #endif /* _PYCSDL2_DISTUTILS_H_ */
