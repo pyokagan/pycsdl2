c9540c5423198c90e702e030513a3867ebf13920
Paul Tan <pyokagan@pyokagan.name>
2015-04-14 19:45:32 +0800

    distutils.h: introduce PyCSDL2_GetSystemSDLExtraLinkArgs()
    To save the list of distutils.extension.Extension.extra_link_args that
    is required to link the csdl2 extension against the system SDL2 library,
    we introduce:
    
    * the PYCSDL2_EXTRA_LINK_ARGS macro, which should be defined in a
      compile flag with the value of extra_link_args.
    
    * PyCSDL2_GetSystemSDLExtraLinkArgs() function to retrieve the saved
      value of PYCSDL2_EXTRA_LINK_ARGS as a PyListObject.

--- src/distutils.h
+++ src/distutils.h
@@ -366,4 +366,49 @@ PyCSDL2_GetSystemSDLRuntimeLibraryDirs(void)
     return list;
 }
 
+#ifndef PYCSDL2_EXTRA_LINK_ARGS
+/**
+ * \def PYCSDL2_EXTRA_LINK_ARGS
+ * \brief \c extra_link_args used by pycsdl2 to link with the system SDL2
+ *        library.
+ *
+ * A comma-separated list of C string literals that must end with a comma. Each
+ * C string literal is an argument passed to the linker for pycsdl2 to link
+ * with the system SDL2 library. This corresponds to the
+ * \c distutils.extension.Extension.extra_link_args attribute. If the list has
+ * no elements, do not define this macro.
+ */
+#define PYCSDL2_EXTRA_LINK_ARGS
+#endif /* PYCSDL2_EXTRA_LINK_ARGS */
+
+/**
+ * \brief Returns PyListObject of PYCSDL2_EXTRA_LINK_ARGS.
+ *
+ * \return PyListObject of PYCSDL2_EXTRA_LINK_ARGS if defined, else an empty
+ *         PyListObject. Returns NULL if an exception occurred.
+ */
+static PyObject *
+PyCSDL2_GetSystemSDLExtraLinkArgs(void)
+{
+    static const char *extra_link_args[] = {PYCSDL2_EXTRA_LINK_ARGS NULL};
+    PyObject *list;
+    Py_ssize_t len, i;
+
+    /* Calculate len of extra_link_args */
+    for (len = 0; extra_link_args[len] != NULL; ++len) {}
+    /* Create output list */
+    if (!(list = PyList_New(len)))
+        return NULL;
+    /* Set items on list */
+    for (i = 0; extra_link_args[i] != NULL; ++i) {
+        PyObject *str = PyUnicode_FromString(extra_link_args[i]);
+        if (str == NULL) {
+            Py_DECREF(list);
+            return NULL;
+        }
+        PyList_SET_ITEM(list, i, str);
+    }
+    return list;
+}
+
 #endif /* _PYCSDL2_DISTUTILS_H_ */
