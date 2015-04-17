69b785d51488c8bf9b8f4ae955a55bad629e7fd6
Paul Tan <pyokagan@pyokagan.name>
2015-04-17 18:59:52 +0800

    error.h: introduce error.h for SDL_error.h bindings
    src/error.h will contain the code to implement bindings for the API
    functions declared in SDL_error.h.

--- src/error.h
+++ src/error.h
@@ -19,40 +19,16 @@
  *     3. This notice may not be removed or altered from any source
  *        distribution.
  */
-/**
- * \file methods.h
- * \brief csdl2 list of module methods.
- */
-#ifndef _PYCSDL2_METHODS_H_
-#define _PYCSDL2_METHODS_H_
-#include <Python.h>
-#include "../include/pycsdl2.h"
-#include "distutils.h"
-#include "error.h"
-
-/**
- * \brief csdl2 methods definitions.
+/*!
+ * \file error.h
+ * \brief Bindings for SDL_error.h
  *
- * \sa PyCSDL2_Module
+ * Implements bindings for SDL's error handling API (SDL_error.h).
  */
-static PyMethodDef PyCSDL2_Methods[] = {
+#ifndef _PYCSDL2_ERROR_H_
+#define _PYCSDL2_ERROR_H_
+#include <Python.h>
+#include <SDL_error.h>
+#include "../include/pycsdl2.h"
 
-    /* distutils.h */
-
-    {"PyCSDL2_GetSystemSDL",
-     (PyCFunction) PyCSDL2_GetSystemSDL,
-     METH_VARARGS | METH_KEYWORDS,
-     "PyCSDL2_GetSystemSDL() -> dict or None\n"
-     "\n"
-     "Returns a dict with the keys 'include_dirs', 'define_macros',\n"
-     "'undef_macros', 'extra_compile_args', 'library_dirs', 'libraries',\n"
-     "'runtime_library_dirs' and 'extra_link_args' corresponding to\n"
-     "attributes of the same name in distutils.extension.Extension which\n"
-     "were used to link csdl2 against the system's SDL2 library. Returns\n"
-     "None if csdl2 was statically linked to SDL2."},
-
-    /* End */
-    {0}
-};
-
-#endif /* _PYCSDL2_METHODS_H_ */
+#endif /* _PYCSDL2_ERROR_H_ */
