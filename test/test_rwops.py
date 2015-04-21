4ea16eba3075aa5280d6d0174c36f65944743b56
Paul Tan <pyokagan@pyokagan.name>
2015-04-21 14:03:05 +0800

    test_rwops: introduce test/test_rwops.py

--- test/test_rwops.py
+++ test/test_rwops.py
@@ -1,19 +1,21 @@
-"""entry point to run all tests"""
+"""test bindings in src/rwops.h"""
 import distutils.util
 import os.path
 import sys
 import unittest
 
 
+tests_dir = os.path.dirname(os.path.abspath(__file__))
+
+
 if __name__ == '__main__':
     plat_specifier = 'lib.{0}-{1}'.format(distutils.util.get_platform(),
                                           sys.version[0:3])
-    tests_dir = os.path.dirname(os.path.abspath(__file__))
     sys.path.insert(0, os.path.join(tests_dir, '..', 'build', plat_specifier))
-    from .test_distutils import *
-    from .test_error import *
-    from .test_events import *
-    from .test_init import *
-    from .test_rwops import *
-    from .test_video import *
+
+
+from csdl2 import *
+
+
+if __name__ == '__main__':
     unittest.main()
