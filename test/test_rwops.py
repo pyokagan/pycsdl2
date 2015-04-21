31d076f83b7e6e64b40ccb99a2d9203052343f2b
Paul Tan <pyokagan@pyokagan.name>
2015-04-21 14:03:18 +0800

    test_rwops: TestRwopsConstants

--- test/test_rwops.py
+++ test/test_rwops.py
@@ -17,5 +17,36 @@ if __name__ == '__main__':
 from csdl2 import *
 
 
+class TestRwopsConstants(unittest.TestCase):
+    """Tests for value of constants in SDL_rwops.h"""
+
+    def test_SDL_RWOPS_UNKNOWN(self):
+        self.assertEqual(SDL_RWOPS_UNKNOWN, 0)
+
+    def test_SDL_RWOPS_WINFILE(self):
+        self.assertEqual(SDL_RWOPS_WINFILE, 1)
+
+    def test_SDL_RWOPS_STDFILE(self):
+        self.assertEqual(SDL_RWOPS_STDFILE, 2)
+
+    def test_SDL_RWOPS_JNIFILE(self):
+        self.assertEqual(SDL_RWOPS_JNIFILE, 3)
+
+    def test_SDL_RWOPS_MEMORY(self):
+        self.assertEqual(SDL_RWOPS_MEMORY, 4)
+
+    def test_SDL_RWOPS_MEMORY_RO(self):
+        self.assertEqual(SDL_RWOPS_MEMORY_RO, 5)
+
+    def test_RW_SEEK_SET(self):
+        self.assertEqual(RW_SEEK_SET, 0)
+
+    def test_RW_SEEK_CUR(self):
+        self.assertEqual(RW_SEEK_CUR, 1)
+
+    def test_RW_SEEKEND(self):
+        self.assertEqual(RW_SEEK_END, 2)
+
+
 if __name__ == '__main__':
     unittest.main()
