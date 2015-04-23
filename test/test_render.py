468bc376cea52fa809cbd69e88212d6d117c4909
Paul Tan <pyokagan@pyokagan.name>
2015-04-23 17:24:58 +0800

    test_render: test value of SDL_render.h constants

--- test/test_render.py
+++ test/test_render.py
@@ -17,5 +17,48 @@ if __name__ == '__main__':
 from csdl2 import *
 
 
+class TestRenderConstants(unittest.TestCase):
+    """Test value of constants defined in SDL_render.h"""
+
+    def test_SDL_RENDERER_SOFTWARE(self):
+        self.assertEqual(SDL_RENDERER_SOFTWARE, 0x01)
+
+    def test_SDL_RENDERER_ACCELERATED(self):
+        self.assertEqual(SDL_RENDERER_ACCELERATED, 0x02)
+
+    def test_SDL_RENDERER_PRESENTVSYNC(self):
+        self.assertEqual(SDL_RENDERER_PRESENTVSYNC, 0x04)
+
+    def test_SDL_RENDERER_TARGETTEXTURE(self):
+        self.assertEqual(SDL_RENDERER_TARGETTEXTURE, 0x08)
+
+    def test_SDL_TEXTUREACCESS_STATIC(self):
+        self.assertEqual(SDL_TEXTUREACCESS_STATIC, 0)
+
+    def test_SDL_TEXTUREACCESS_STREAMING(self):
+        self.assertEqual(SDL_TEXTUREACCESS_STREAMING, 1)
+
+    def test_SDL_TEXTUREACCESS_TARGET(self):
+        self.assertEqual(SDL_TEXTUREACCESS_TARGET, 2)
+
+    def test_SDL_TEXTUREMODULATE_NONE(self):
+        self.assertEqual(SDL_TEXTUREMODULATE_NONE, 0)
+
+    def test_SDL_TEXTUREMODULATE_COLOR(self):
+        self.assertEqual(SDL_TEXTUREMODULATE_COLOR, 1)
+
+    def test_SDL_TEXTUREMODULATE_ALPHA(self):
+        self.assertEqual(SDL_TEXTUREMODULATE_ALPHA, 2)
+
+    def test_SDL_FLIP_NONE(self):
+        self.assertEqual(SDL_FLIP_NONE, 0)
+
+    def test_SDL_FLIP_HORIZONTAL(self):
+        self.assertEqual(SDL_FLIP_HORIZONTAL, 0x01)
+
+    def test_SDL_FLIP_VERTICAL(self):
+        self.assertEqual(SDL_FLIP_VERTICAL, 0x02)
+
+
 if __name__ == '__main__':
     unittest.main()
