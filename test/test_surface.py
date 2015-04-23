2c35e19b676efa4decdba396f35a4db22afe42a8
Paul Tan <pyokagan@pyokagan.name>
2015-04-23 11:20:58 +0800

    test_surface: test for constants in SDL_surface.h

--- test/test_surface.py
+++ test/test_surface.py
@@ -17,5 +17,21 @@ if __name__ == '__main__':
 from csdl2 import *
 
 
+class TestSurfaceConstants(unittest.TestCase):
+    """Tests value of constants in SDL_surface.h"""
+
+    def test_SDL_SWSURFACE(self):
+        self.assertEqual(SDL_SWSURFACE, 0)
+
+    def test_SDL_PREALLOC(self):
+        self.assertEqual(SDL_PREALLOC, 0x01)
+
+    def test_SDL_RLEACCEL(self):
+        self.assertEqual(SDL_RLEACCEL, 0x02)
+
+    def test_SDL_DONTFREE(self):
+        self.assertEqual(SDL_DONTFREE, 0x04)
+
+
 if __name__ == '__main__':
     unittest.main()
