c62cc0266a5ba6d8ebcd8fec5e478a2d27bc5eb4
Paul Tan <pyokagan@pyokagan.name>
2015-05-03 15:24:55 +0800

    PyCSDL2_initaudio(): add SDL_audio.h constants to module

--- src/audio.h
+++ src/audio.h
@@ -37,12 +37,67 @@
 /**
  * \brief Initializes bindings to SDL_audio.h
  *
+ * Adds constants defined in SDL_audio.h to module.
+ *
  * \param module csdl2 module object
  * \returns 1 on success, 0 if an exception occurred.
  */
 static int
 PyCSDL2_initaudio(PyObject *module)
 {
+    static const PyCSDL2_Constant constants[] = {
+        {"SDL_AUDIO_MASK_BITSIZE", SDL_AUDIO_MASK_BITSIZE},
+        {"SDL_AUDIO_MASK_DATATYPE", SDL_AUDIO_MASK_DATATYPE},
+        {"SDL_AUDIO_MASK_ENDIAN", SDL_AUDIO_MASK_ENDIAN},
+        {"SDL_AUDIO_MASK_SIGNED", SDL_AUDIO_MASK_SIGNED},
+
+        /* Audio format flags */
+        {"AUDIO_U8", AUDIO_U8},
+        {"AUDIO_S8", AUDIO_S8},
+        {"AUDIO_U16LSB", AUDIO_U16LSB},
+        {"AUDIO_S16LSB", AUDIO_S16LSB},
+        {"AUDIO_U16MSB", AUDIO_U16MSB},
+        {"AUDIO_S16MSB", AUDIO_S16MSB},
+        {"AUDIO_U16", AUDIO_U16},
+        {"AUDIO_S16", AUDIO_S16},
+
+        /* int32 support */
+        {"AUDIO_S32LSB", AUDIO_S32LSB},
+        {"AUDIO_S32MSB", AUDIO_S32MSB},
+        {"AUDIO_S32", AUDIO_S32},
+
+        /* float32 support */
+        {"AUDIO_F32LSB", AUDIO_F32LSB},
+        {"AUDIO_F32MSB", AUDIO_F32MSB},
+        {"AUDIO_F32", AUDIO_F32},
+
+        /* Native audio byte ordering */
+        {"AUDIO_U16SYS", AUDIO_U16SYS},
+        {"AUDIO_S16SYS", AUDIO_S16SYS},
+        {"AUDIO_S32SYS", AUDIO_S32SYS},
+        {"AUDIO_F32SYS", AUDIO_F32SYS},
+
+        /* Allow change flags */
+        {"SDL_AUDIO_ALLOW_FREQUENCY_CHANGE", SDL_AUDIO_ALLOW_FREQUENCY_CHANGE},
+        {"SDL_AUDIO_ALLOW_FORMAT_CHANGE", SDL_AUDIO_ALLOW_FORMAT_CHANGE},
+        {"SDL_AUDIO_ALLOW_CHANNELS_CHANGE", SDL_AUDIO_ALLOW_CHANNELS_CHANGE},
+        {"SDL_AUDIO_ALLOW_ANY_CHANGE", SDL_AUDIO_ALLOW_ANY_CHANGE},
+
+        /* Audio state */
+        {"SDL_AUDIO_STOPPED", SDL_AUDIO_STOPPED},
+        {"SDL_AUDIO_PLAYING", SDL_AUDIO_PLAYING},
+        {"SDL_AUDIO_PAUSED", SDL_AUDIO_PAUSED},
+
+        {"SDL_MIX_MAXVOLUME", SDL_MIX_MAXVOLUME},
+
+        {NULL, 0}
+    };
+    const PyCSDL2_Constant *c;
+
+    for (c = constants; c->name; c++)
+        if (PyModule_AddIntConstant(module, c->name, c->value))
+            return 0;
+
     return 1;
 }
 
