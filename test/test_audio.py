78b6e4b522b9ea23316f84c8ae4dfef87e6ec3ff
Paul Tan <pyokagan@pyokagan.name>
2015-05-03 15:28:43 +0800

    tests: introduce test_audio.py

---
+++ test/test_audio.py
@@ -0,0 +1,21 @@
+"""test bindings in src/audio.h"""
+import distutils.util
+import os.path
+import sys
+import unittest
+
+
+tests_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+if __name__ == '__main__':
+    plat_specifier = 'lib.{0}-{1}'.format(distutils.util.get_platform(),
+                                          sys.version[0:3])
+    sys.path.insert(0, os.path.join(tests_dir, '..', 'build', plat_specifier))
+
+
+from csdl2 import *
+
+
+if __name__ == '__main__':
+    unittest.main()
